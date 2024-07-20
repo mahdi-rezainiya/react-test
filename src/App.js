80a0eb320a598a3be1904db35548c16fcd7017c4
mahdi <mahdi.rezainiya@gmail.com>
2024-07-20 16:48:15 +0330

    new changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import './App.css';
-import { BrowserRouter , Routes, Route , NavLink } from 'react-router-dom'
-
-import About from './pages/About';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Articles from './pages/Articles';
-// import { useState } from 'react';
 
 function App() {
-  // const [loggedIn] = useState(false);
   return (
   <div className="App">
-    <BrowserRouter>
-    <nav>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/About'>About</NavLink>
-      <NavLink to='/test'>Test</NavLink>
-      <NavLink to='/contact'>Contact</NavLink>
-    </nav>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/about/*' element={<About/>} />
-        <Route path='/articles/:id' element={<Articles/>} />
-      </Routes>    
-    </BrowserRouter>
+
   </div>
   );
 }
--- src/pages/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from 'react' ;
-export default function Footer() {
-    return (
-    <h1>Mahdi Rezainiya is the best</h1>
-    )
-}
